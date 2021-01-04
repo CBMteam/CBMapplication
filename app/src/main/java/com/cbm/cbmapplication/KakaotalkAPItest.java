3ea1b85cd3da945130a8d46b7d5a32d7611f897a
kha-github <rlahyuna1234@gmail.com>
2021-01-05 03:54:54 +0900

    Connect with PHP

---
+++ app/src/main/java/com/cbm/cbmapplication/KakaotalkAPItest.java
@@ -0,0 +1,15 @@
+package com.cbm.cbmapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KakaotalkAPItest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kakaotalkapitest);
+
+    }
+}
