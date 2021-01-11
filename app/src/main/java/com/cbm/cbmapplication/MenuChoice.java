d955b9cd6ea675f8db76a971deadbcd11f795069
kha-github <rlahyuna1234@gmail.com>
2021-01-12 05:54:42 +0900

    Update Kakao API & Login

---
+++ app/src/main/java/com/cbm/cbmapplication/MenuChoice.java
@@ -0,0 +1,60 @@
+package com.cbm.cbmapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuChoice extends AppCompatActivity {
+
+    TextView txtView;
+    Intent intent = getIntent();
+
+    // String name = intent.getStringExtra("name");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menuchoice);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), KakaotalkAPItest.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapAPItest.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CalendarAPItest.class);
+                startActivity(intent);
+            }
+        });
+
+        // txtView.setText(name);
+
+
+
+    }
+}
