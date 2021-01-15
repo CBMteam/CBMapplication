e95336db003415b14f0e65e2444ed48e24a5c236
s2moon98 <s2moon98@naver.com>
2021-01-15 20:33:12 +0900

    MPAndroidChart api

---
+++ app/src/main/java/com/cbm/cbmapplication/Chart.java
@@ -0,0 +1,53 @@
+package com.cbm.cbmapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Chart extends AppCompatActivity {
+    private LineChart chart;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bloodglucose_chart);
+
+        chart = findViewById(R.id.linechart);
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+
+            float val = (float) (Math.random() * 10);
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+        set1 = new LineDataSet(values, "DataSet 1");
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1); // add the data sets
+
+        // create a data object with the data sets
+        LineData data = new LineData(dataSets);
+
+        // black lines and points
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+
+        // set data
+        chart.setData(data);
+    }
+
+
+}
