6ca8b682129f90c8eff428f87bccb36e480b3156
kha-github <rlahyuna1234@gmail.com>
2021-04-16 01:49:38 +0900

    update mainmenu:add chart

--- app/src/main/java/com/cbm/cbmapplication/MenuChoice.java
+++ app/src/main/java/com/cbm/cbmapplication/MenuChoice.java
@@ -1,17 +1,27 @@
 package com.cbm.cbmapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MenuChoice extends AppCompatActivity {
 
-    TextView txtView;
+    private LineChart chart;
+    TextView ml_tteresult;
     Intent intent = getIntent();
 
     // String name = intent.getStringExtra("name");
@@ -22,6 +32,8 @@ public class MenuChoice extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuchoice);
 
+        ml_tteresult = (TextView) findViewById(R.id.ml_tteresult);
+        ml_tteresult.setText((int)(Math.random()*100)+" 분 뒤");
         ImageButton button = (ImageButton) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -63,17 +75,6 @@ public class MenuChoice extends AppCompatActivity {
             }
         });
 
-        Button button5 = (Button) findViewById(R.id.chartBtn);
-        button5.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                System.out.println("버튼누름");
-                Intent intent = new Intent(getApplicationContext(), Chart.class);
-                startActivity(intent);
-            }
-        });
-
         Button button6 = (Button) findViewById(R.id.modelBtn);
         button6.setOnClickListener(new View.OnClickListener() {
 
@@ -87,6 +88,33 @@ public class MenuChoice extends AppCompatActivity {
         // txtView.setText(name);
 
 
+        // draw chart
+
+        chart = findViewById(R.id.cgmchart);
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+
+            float val = (float) (Math.random() * 10);
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+        set1 = new LineDataSet(values, "DataSet 1");
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1); // add the data sets
+
+        // create a data object with the data sets
+        LineData data = new LineData(dataSets);
+
+        // black lines and points
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+
+        // set data
+        chart.setData(data);
 
     }
 }
