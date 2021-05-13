22bef51a20e26802560d7ce62fb5c5f049c24de9
kha-github <rlahyuna1234@gmail.com>
2021-05-13 22:16:48 +0900

    complete calendar schedule list

--- app/src/main/java/com/cbm/cbmapplication/Calendar.java
+++ app/src/main/java/com/cbm/cbmapplication/Calendar.java
@@ -43,7 +43,7 @@ public class Calendar extends AppCompatActivity {
     CalendarView calendarView;
     int selectYear,selectMonth,selectDay;
     Button btnadd,btndel;
-    private ListView callist;
+    static private ListView callist;
     private ListViewAdpater adapter;
     String send;
     String content;
@@ -83,6 +83,21 @@ public class Calendar extends AppCompatActivity {
         selectMonth=Integer.parseInt(Month.format(CurDate));
         selectDay=Integer.parseInt(Day.format(CurDate));
 
+
+
+        adapter.clear();
+        adapter.notifyDataSetChanged();
+
+
+        String date=selectYear+"-"+selectMonth+"-"+selectDay;
+
+        getCarbTask carbtask=new getCarbTask(Calendar.this);
+        carbtask.execute("http://"+IP_ADDRESS+"/getcarb.php",user_email,date);
+
+        getInsulinTask insulintask=new getInsulinTask(Calendar.this);
+        insulintask.execute("http://"+IP_ADDRESS+"/getinsulin.php",user_email,date);
+
+
         //날짜 선택할 경우
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -91,6 +106,19 @@ public class Calendar extends AppCompatActivity {
                 selectYear=year;
                 selectMonth=month+1;
                 selectDay=dayOfMonth;
+
+
+                adapter.clear();
+                adapter.notifyDataSetChanged();
+
+                String date=selectYear+"-"+selectMonth+"-"+selectDay;
+                Log.d("datecheck",date);
+
+                getCarbTask carbtask=new getCarbTask(Calendar.this);
+                carbtask.execute("http://"+IP_ADDRESS+"/getcarb.php",user_email,date);
+
+                getInsulinTask insulintask=new getInsulinTask(Calendar.this);
+                insulintask.execute("http://"+IP_ADDRESS+"/getinsulin.php",user_email,date);
 
             }
         });
@@ -103,12 +131,8 @@ public class Calendar extends AppCompatActivity {
         if(Integer.toString(selectDay).length()==1){
             day="0"+selectDay;
         }
-        String date=selectYear+"-"+month+"-"+day;
-        Log.d("datecheck",date);
 
 
-        getCalendarListTask task=new getCalendarListTask(Calendar.this);
-        task.execute("http://"+IP_ADDRESS+"/getcarb.php",user_email,date);
 
         btnadd.setOnClickListener(new View.OnClickListener() {
 
@@ -167,6 +191,9 @@ public class Calendar extends AppCompatActivity {
                                 task.execute("http://"+IP_ADDRESS+"/deletecalendarlist.php",user_email,date);
 
                                 Toast.makeText(getApplicationContext(),"일정이 삭제되었습니다",Toast.LENGTH_SHORT).show();
+
+                                adapter.clear();
+                                adapter.notifyDataSetChanged();
                             }
                         });
 
@@ -189,14 +216,13 @@ public class Calendar extends AppCompatActivity {
 
     }
 
+    public class deleteCalendarListTask extends AsyncTask<String, Void, String> {
 
-    public class getCalendarListTask extends AsyncTask<String, Void, String> {
-
-        private static final String TAG = "getCalendarListTask";
+        private static final String TAG = "deleteCalendarListTask";
 
         public Context mContext;
 
-        public getCalendarListTask(Context mContext) {
+        public deleteCalendarListTask(Context mContext) {
             super();
 
             this.mContext = mContext;
@@ -299,13 +325,14 @@ public class Calendar extends AppCompatActivity {
 
     }
 
-    public class deleteCalendarListTask extends AsyncTask<String, Void, String> {
 
-        private static final String TAG = "deleteCalendarListTask";
+    public class getCarbTask extends AsyncTask<String, Void, String> {
+
+        private static final String TAG = "getCarbTask";
 
         public Context mContext;
 
-        public deleteCalendarListTask(Context mContext) {
+        public getCarbTask(Context mContext) {
             super();
 
             this.mContext = mContext;
@@ -326,7 +353,118 @@ public class Calendar extends AppCompatActivity {
             if (result == null) {
                 Log.d(TAG, "result is null - ");
             } else {
-                showResult(result); //JSON 형태의 결과를 처리
+                showResultCarb(result); //JSON 형태의 결과를 처리
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않습니다.
+            String serverURL = (String) params[0];
+
+            // TODO : 아래 형식처럼 원하는 key과 value를 계속 추가시킬수있다.
+            // 1. PHP 파일을 실행시킬 수 있는 주소와 전송할 데이터를 준비합니다.
+            String user_email = (String) params[1];
+            String user_date = (String) params[2];
+
+            // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
+            // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
+            // 여기에 적어준 이름을 나중에 PHP에서 사용하여 값을 얻게 됩니다.
+
+            // TODO : 위에 추가한 형식처럼 아래 postParameters에 key과 value를 계속 추가시키면 끝이다.
+            // ex : String postParameters = "name=" + name + "&country=" + country;
+            String postParameters = "user_email="+user_email+"&user_date="+user_date;
+
+            Log.d(TAG, postParameters);
+
+            StringBuilder jsonHtml = new StringBuilder();
+
+            try {
+                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
+                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);//5초안에 응답이 오지 않으면 예외가 발생합니다.
+                httpURLConnection.setConnectTimeout(5000);//5초안에 연결이 안되면 예외가 발생합니다.
+                httpURLConnection.setRequestMethod("POST");//요청 방식을 POST로 합니다.
+                httpURLConnection.connect(); //전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes(StandardCharsets.UTF_8));//전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
+                outputStream.flush();
+                outputStream.close();
+
+                // 3. 응답을 읽습니다.
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) // 정상적인 응답 데이터
+                {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {  // 에러 발생
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                // 4. StringBuilder를 사용하여 수신되는 데이터를 저장합니다.
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                String data = sb.toString().trim();
+
+                return data;
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "getPets: Error ", e);
+                return new String("Error: " + e.getMessage());
+            }
+        }
+
+
+    }
+
+
+
+    public class getInsulinTask extends AsyncTask<String, Void, String> {
+
+        private static final String TAG = "getInsulinTask";
+
+        public Context mContext;
+
+        public getInsulinTask(Context mContext) {
+            super();
+
+            this.mContext = mContext;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        /* http 연결 진행이 끝나고 난 후에 사용되는곳 (result -> http 연결 후 결과) */
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.d(TAG, "response - " + result); //로그로 결과 우선 확인
+            if (result == null) {
+                Log.d(TAG, "result is null - ");
+            } else {
+                showResultInsulin(result); //JSON 형태의 결과를 처리
             }
         }
 
@@ -451,5 +589,88 @@ public class Calendar extends AppCompatActivity {
         }
     }
 
+
+    private void showResultCarb(String mJsonString) {
+
+        String TAG_JSON = "carb_list"; //어레이 이름
+        /*php 파일에서 넘긴 json 이름들*/
+        String TAG_DATETIME = "fDate";
+        String TAG_CARB = "carb";
+
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            //JSON으로 푸시한 전체 데이터 가져오기
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject item = jsonArray.getJSONObject(i); //하나 가져오기
+
+                String result_datetime = item.getString(TAG_DATETIME);
+                String result_carb = item.getString(TAG_CARB);
+
+                adapter.addItem("식사량 " + result_carb, result_datetime);
+
+                adapter.notifyDataSetChanged();
+
+
+            }
+        } catch (JSONException e) {
+            //Log.d(TAG, "showResult : ", e);
+        }
+    }
+
+
+    private void showResultInsulin(String mJsonString) {
+
+        String TAG_JSON = "insulin_list"; //어레이 이름
+        /*php 파일에서 넘긴 json 이름들*/
+        String TAG_DATETIME = "fDate";
+        String TAG_INSULIN = "insulin";
+
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            //JSON으로 푸시한 전체 데이터 가져오기
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject item = jsonArray.getJSONObject(i); //하나 가져오기
+
+                String result_datetime = item.getString(TAG_DATETIME);
+                String result_insulin = item.getString(TAG_INSULIN);
+
+                adapter.addItem("인슐린 " + result_insulin,result_datetime);
+
+                adapter.notifyDataSetChanged();
+
+            }
+        } catch (JSONException e) {
+            //Log.d(TAG, "showResult : ", e);
+        }
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        adapter.clear();
+        adapter.notifyDataSetChanged();
+
+        String date=selectYear+"-"+selectMonth+"-"+selectDay;
+        Log.d("datecheck",date);
+
+        getCarbTask carbtask=new getCarbTask(Calendar.this);
+        carbtask.execute("http://"+IP_ADDRESS+"/getcarb.php",user_email,date);
+
+        getInsulinTask insulintask=new getInsulinTask(Calendar.this);
+        insulintask.execute("http://"+IP_ADDRESS+"/getinsulin.php",user_email,date);
+
+    }
+
+
 }
+
 
