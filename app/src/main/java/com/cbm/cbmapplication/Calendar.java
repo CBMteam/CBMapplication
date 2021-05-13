f9d3e89ff248ba6d890343fea9bf5976fbd8ded5
kha-github <rlahyuna1234@gmail.com>
2021-05-13 16:44:59 +0900

    connect chart data, calender api

--- app/src/main/java/com/cbm/cbmapplication/Calendar.java
+++ app/src/main/java/com/cbm/cbmapplication/Calendar.java
@@ -151,6 +151,7 @@ public class Calendar extends AppCompatActivity {
                         .setPositiveButton("아니오", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
+
                                 dialogInterface.cancel();
                             }
                         })
@@ -158,6 +159,13 @@ public class Calendar extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 //일정 삭제 구현
+
+
+                                String date=Integer.toString(selectYear)+"-"+Integer.toString(selectMonth)+"-"+Integer.toString(selectDay);
+
+                                deleteCalendarListTask task=new deleteCalendarListTask(Calendar.this);
+                                task.execute("http://"+IP_ADDRESS+"/deletecalendarlist.php",user_email,date);
+
                                 Toast.makeText(getApplicationContext(),"일정이 삭제되었습니다",Toast.LENGTH_SHORT).show();
                             }
                         });
@@ -223,7 +231,116 @@ public class Calendar extends AppCompatActivity {
             // TODO : 아래 형식처럼 원하는 key과 value를 계속 추가시킬수있다.
             // 1. PHP 파일을 실행시킬 수 있는 주소와 전송할 데이터를 준비합니다.
             String user_email = (String) params[1];
-            String user_date = (String) params[1];
+            String user_date = (String) params[2];
+
+            // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
+            // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
+            // 여기에 적어준 이름을 나중에 PHP에서 사용하여 값을 얻게 됩니다.
+
+            // TODO : 위에 추가한 형식처럼 아래 postParameters에 key과 value를 계속 추가시키면 끝이다.
+            // ex : String postParameters = "name=" + name + "&country=" + country;
+            String postParameters = "user_email="+user_email+"&user_date="+user_date;
+
+            Log.d(TAG, postParameters);
+
+            StringBuilder jsonHtml = new StringBuilder();
+
+            try {
+                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
+                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);//5초안에 응답이 오지 않으면 예외가 발생합니다.
+                httpURLConnection.setConnectTimeout(5000);//5초안에 연결이 안되면 예외가 발생합니다.
+                httpURLConnection.setRequestMethod("POST");//요청 방식을 POST로 합니다.
+                httpURLConnection.connect(); //전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes(StandardCharsets.UTF_8));//전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
+                outputStream.flush();
+                outputStream.close();
+
+                // 3. 응답을 읽습니다.
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) // 정상적인 응답 데이터
+                {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {  // 에러 발생
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                // 4. StringBuilder를 사용하여 수신되는 데이터를 저장합니다.
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                String data = sb.toString().trim();
+
+                return data;
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "getPets: Error ", e);
+                return new String("Error: " + e.getMessage());
+            }
+        }
+
+
+    }
+
+    public class deleteCalendarListTask extends AsyncTask<String, Void, String> {
+
+        private static final String TAG = "deleteCalendarListTask";
+
+        public Context mContext;
+
+        public deleteCalendarListTask(Context mContext) {
+            super();
+
+            this.mContext = mContext;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        /* http 연결 진행이 끝나고 난 후에 사용되는곳 (result -> http 연결 후 결과) */
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.d(TAG, "response - " + result); //로그로 결과 우선 확인
+            if (result == null) {
+                Log.d(TAG, "result is null - ");
+            } else {
+                showResult(result); //JSON 형태의 결과를 처리
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않습니다.
+            String serverURL = (String) params[0];
+
+            // TODO : 아래 형식처럼 원하는 key과 value를 계속 추가시킬수있다.
+            // 1. PHP 파일을 실행시킬 수 있는 주소와 전송할 데이터를 준비합니다.
+            String user_email = (String) params[1];
+            String user_date = (String) params[2];
 
             // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
             // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
@@ -333,107 +450,6 @@ public class Calendar extends AppCompatActivity {
             //Log.d(TAG, "showResult : ", e);
         }
     }
-
-//
-//        public class getdata extends AsyncTask<String, Void, String> {
-//
-//        private static final String TAG = "getdata";
-//
-//        public Context mContext;
-//
-//        public getdata(Context mContext) {
-//            super();
-//
-//            this.mContext = mContext;
-//        }
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//
-//        }
-//
-//        @Override
-//        protected String doInBackground(String... params) {
-//
-//            // POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않습니다.
-//            String serverURL = (String) params[0];
-//
-//            // TODO : 아래 형식처럼 원하는 key과 value를 계속 추가시킬수있다.
-//            // 1. PHP 파일을 실행시킬 수 있는 주소와 전송할 데이터를 준비합니다.
-//            String user_email = (String) params[1];
-//            String user_date = (String) params[2];
-//
-//            // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
-//            // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
-//            // 여기에 적어준 이름을 나중에 PHP에서 사용하여 값을 얻게 됩니다.
-//
-//            // TODO : 위에 추가한 형식처럼 아래 postParameters에 key과 value를 계속 추가시키면 끝이다.
-//            // ex : String postParameters = "name=" + name + "&country=" + country;
-//            String postParameters = "email=" + user_email + "&date=" + user_date;
-//
-//            Log.d(TAG, postParameters);
-//
-//            StringBuilder jsonHtml = new StringBuilder();
-//
-//            try {
-//                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
-//                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
-//
-//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//
-//                if ( conn != null ) {
-//                    conn.setConnectTimeout(10000);
-//                    conn.setUseCaches(false);
-//
-//                    if ( conn.getResponseCode() == HttpURLConnection.HTTP_OK ) {
-//                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-//                        while (true) {
-//                            String line = br.readLine();
-//                            if (line == null)
-//                                break;
-//                            jsonHtml.append(line + "\n");
-//                        }
-//                        br.close();
-//
-//                    }
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                Log.d(TAG, "InsertData: Error ", e);
-//            }
-//            return jsonHtml.toString();
-//        }
-//
-//        protected void onPostExecute(String str) {
-//            try {
-//                // PHP에서 받아온 JSON 데이터를 JSON오브젝트로 변환
-//                JSONObject jObject = new JSONObject(str);
-//                // results라는 key는 JSON배열로 되어있다.
-//                JSONArray results = jObject.getJSONArray("results");
-//                String zz = "";
-//                zz += "Status : " + jObject.get("status");
-//                zz += "\n";
-//                zz += "Number of results : " + jObject.get("num_result");
-//                zz += "\n";
-//                zz += "Results : \n";
-//
-//                String calcontent;
-//
-//                for ( int i = 0; i < results.length(); ++i ) {
-//                    JSONObject temp = results.getJSONObject(i);
-//                    calcontent = temp.get("carb").toString();
-//                    adapter.addItem("식사량",calcontent);
-//                    zz += "\temail : " + temp.get("email");
-//                    zz += "\tdate : " + temp.get("date");
-//                }
-//                Log.d(TAG,zz);
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//    }
 
 }
 
