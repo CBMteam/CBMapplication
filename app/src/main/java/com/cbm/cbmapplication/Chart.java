572093983576a91bc46fcf6df082bc5ea6163c2b
s2moon98 <s2moon98@naver.com>
2021-05-13 18:06:19 +0900

    get tte from server

--- app/src/main/java/com/cbm/cbmapplication/Chart.java
+++ app/src/main/java/com/cbm/cbmapplication/Chart.java
@@ -38,148 +38,16 @@ public class Chart extends AppCompatActivity {
 
         chart = findViewById(R.id.linechart);
 
+        ArrayList<Entry> values = new ArrayList<>();
 
-    }
+        for (int i = 0; i < 10; i++) {
 
-    public class getFriendListTask extends AsyncTask<String, Void, String> {
-
-        private static final String TAG = "getFriendListTask";
-
-        public Context mContext;
-
-        public getFriendListTask(Context mContext) {
-            super();
-
-            this.mContext = mContext;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-        /* http 연결 진행이 끝나고 난 후에 사용되는곳 (result -> http 연결 후 결과) */
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            Log.d(TAG, "response - " + result); //로그로 결과 우선 확인
-            if (result == null) {
-                Log.d(TAG, "result is null - ");
-            } else {
-                showResult(result); //JSON 형태의 결과를 처리
-            }
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            // POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않습니다.
-            String serverURL = (String) params[0];
-
-            // TODO : 아래 형식처럼 원하는 key과 value를 계속 추가시킬수있다.
-            // 1. PHP 파일을 실행시킬 수 있는 주소와 전송할 데이터를 준비합니다.
-            String user_email = (String) params[1];
-
-            // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
-            // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
-            // 여기에 적어준 이름을 나중에 PHP에서 사용하여 값을 얻게 됩니다.
-
-            // TODO : 위에 추가한 형식처럼 아래 postParameters에 key과 value를 계속 추가시키면 끝이다.
-            // ex : String postParameters = "name=" + name + "&country=" + country;
-            String postParameters = "user_email="+user_email;
-
-            Log.d(TAG, postParameters);
-
-            StringBuilder jsonHtml = new StringBuilder();
-
-            try {
-                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
-                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
-
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-                httpURLConnection.setReadTimeout(5000);//5초안에 응답이 오지 않으면 예외가 발생합니다.
-                httpURLConnection.setConnectTimeout(5000);//5초안에 연결이 안되면 예외가 발생합니다.
-                httpURLConnection.setRequestMethod("POST");//요청 방식을 POST로 합니다.
-                httpURLConnection.connect(); //전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
-
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                outputStream.write(postParameters.getBytes(StandardCharsets.UTF_8));//전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
-                outputStream.flush();
-                outputStream.close();
-
-                // 3. 응답을 읽습니다.
-                int responseStatusCode = httpURLConnection.getResponseCode();
-                Log.d(TAG, "POST response code - " + responseStatusCode);
-
-                InputStream inputStream;
-                if (responseStatusCode == HttpURLConnection.HTTP_OK) // 정상적인 응답 데이터
-                {
-                    inputStream = httpURLConnection.getInputStream();
-                } else {  // 에러 발생
-                    inputStream = httpURLConnection.getErrorStream();
-                }
-
-                // 4. StringBuilder를 사용하여 수신되는 데이터를 저장합니다.
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-
-                while ((line = bufferedReader.readLine()) != null) {
-                    sb.append(line);
-                }
-                bufferedReader.close();
-                String data = sb.toString().trim();
-
-                return data;
-
-            } catch (Exception e) {
-
-                Log.d(TAG, "getPets: Error ", e);
-                return new String("Error: " + e.getMessage());
-            }
-        }
-
-
-    }
-
-
-    private void showResult(String mJsonString) {
-
-
-        String TAG_JSON = "bloodsugar_list"; //어레이 이름
-        /*php 파일에서 넘긴 json 이름들*/
-        String BLOODSUGAR = "bloodsugar";
-
-        ArrayList<Entry> cgm_value_list = new ArrayList<>();
-
-        try {
-            JSONObject jsonObject = new JSONObject(mJsonString);
-            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
-
-            //JSON으로 푸시한 전체 데이터 가져오기
-            for (int i = 0; i < jsonArray.length(); i++) {
-
-                JSONObject item = jsonArray.getJSONObject(i); //하나 가져오기
-
-                String bloodsugar =  item.getString(BLOODSUGAR);
-
-                float val = Integer.parseInt(bloodsugar);
-                cgm_value_list.add(new Entry(i, val));
-
-            }
-        } catch (JSONException e) {
-            //Log.d(TAG, "showResult : ", e);
+            float val = (float) (Math.random() * 10);
+            values.add(new Entry(i, val));
         }
 
         LineDataSet set1;
-        set1 = new LineDataSet(cgm_value_list, "혈당값");
+        set1 = new LineDataSet(values, "DataSet 1");
 
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
         dataSets.add(set1); // add the data sets
@@ -194,8 +62,8 @@ public class Chart extends AppCompatActivity {
         // set data
         chart.setData(data);
 
-        //adapter.getItemList(board_list);
 
     }
+
 
 }
--- app/src/main/java/com/cbm/cbmapplication/MenuChoice.java
+++ app/src/main/java/com/cbm/cbmapplication/MenuChoice.java
@@ -3,9 +3,11 @@ package com.cbm.cbmapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -17,9 +19,19 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MenuChoice extends AppCompatActivity {
+
+    String URL = "https://ad4d33a7ca5f.ngrok.io";
 
     private LineChart chart;
     TextView ml_tteresult;
@@ -30,16 +42,36 @@ public class MenuChoice extends AppCompatActivity {
 
     String user_email;
 
+    DialogGroup dialogGroup = new DialogGroup();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuchoice);
 
-        user_email = PreferenceManager.getString(mContext, "user_email");
+        String tte = "";
+
+        MenuChoice.JoinTask task = new MenuChoice.JoinTask(MenuChoice.this);
+        System.out.println("tte 받으러 가는중");
+        try {
+            String result = task.execute(URL+"/tte").get();
+            System.out.println("시간 : " + result);
+            tte = Integer.toString(Integer.parseInt(result)*5);
+
+            if (result.equals("failure")){
+                dialogGroup.dialogJoinDuplicateId(MenuChoice.this);
+                return;
+            }
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         ml_tteresult = (TextView) findViewById(R.id.ml_tteresult);
-        ml_tteresult.setText((int)(Math.random()*100)+" 분 뒤");
+        ml_tteresult.setText(tte+" 분 뒤");
         ImageButton btn_logout = (ImageButton) findViewById(R.id.logout);
         btn_logout.setOnClickListener(new View.OnClickListener() {
 
@@ -114,4 +146,111 @@ public class MenuChoice extends AppCompatActivity {
         chart.setData(data);
 
     }
+
+    public class JoinTask extends AsyncTask<String, Void, String> {
+
+        private static final String TAG = "InsertTask";
+
+        public Context mContext;
+
+        public JoinTask(Context mContext) {
+            super();
+
+            this.mContext = mContext;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않습니다.
+            String serverURL = (String) params[0];
+
+
+
+            // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
+            // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
+            // 여기에 적어준 이름을 나중에 PHP에서 사용하여 값을 얻게 됩니다.
+
+            // TODO : 위에 추가한 형식처럼 아래 postParameters에 key과 value를 계속 추가시키면 끝이다.
+            // ex : String postParameters = "name=" + name + "&country=" + country;;
+
+            try {
+                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
+                java.net.URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000); //5초안에 응답이 오지 않으면 예외가 발생합니다.
+
+                httpURLConnection.setConnectTimeout(5000); //5초안에 연결이 안되면 예외가 발생합니다.
+
+                httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST로 합니다.
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+
+                outputStream.flush();
+                outputStream.close();
+
+
+                // 응답을 읽습니다.
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+
+                    // 정상적인 응답 데이터
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+
+                    // 에러 발생
+
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+
+
+
 }
