2ef9fc33b2ebc3d314a438697ea339c2dec3616c
s2moon98 <s2moon98@naver.com>
2021-05-16 18:24:40 +0900

    implement add carb, insulin

--- app/src/main/java/com/cbm/cbmapplication/else_record.java
+++ app/src/main/java/com/cbm/cbmapplication/else_record.java
@@ -35,6 +35,7 @@ public class else_record extends AppCompatActivity {
     ImageButton btn_record_goback;
     int selectYear,selectMonth,selectDay,selectHour,selectmin;
     String user_email;
+    String URL = "http://ad4d33a7ca5f.ngrok.io";
 
     //DB
     String IP_ADDRESS="223.194.46.209";//안드에서는 localhost 쓰려면 127.0.0.1 이 아니고 10.0.2.2
@@ -113,10 +114,14 @@ public class else_record extends AppCompatActivity {
                if(choice.equals("인슐린")){
                    else_record.ElseTask task=new else_record.ElseTask(else_record.this);
                    task.execute("http://"+IP_ADDRESS+"/insulininsert.php",user_email,value,date);
+                   else_record.FeatureTask ftask=new else_record.FeatureTask(else_record.this);
+                   ftask.execute(URL+"/insulin/add",user_email,value,date);
                }
                else if(choice.equals("식사량")){
                    else_record.ElseTask task=new else_record.ElseTask(else_record.this);
                    task.execute("http://"+IP_ADDRESS+"/carbinsert.php",user_email,value,date);
+                   else_record.FeatureTask ftask=new else_record.FeatureTask(else_record.this);
+                   ftask.execute(URL+"/carb/add",user_email,value,date);
                }
 
                 //일정 추가 완료시 원래 화면으로
@@ -183,6 +188,117 @@ public class else_record extends AppCompatActivity {
             // TODO : 위에 추가한 형식처럼 아래 postParameters에 key과 value를 계속 추가시키면 끝이다.
             // ex : String postParameters = "name=" + name + "&country=" + country;
             String postParameters = "email=" + user_email + "&value=" + user_value + "&date=" + user_date;
+
+            Log.d(TAG, postParameters);
+
+            try {
+                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
+                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000); //5초안에 응답이 오지 않으면 예외가 발생합니다.
+
+                httpURLConnection.setConnectTimeout(5000); //5초안에 연결이 안되면 예외가 발생합니다.
+
+                httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST로 합니다.
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8")); //전송할 데이터가 저장된 변수를 이곳에 입력합니다.
+
+                outputStream.flush();
+                outputStream.close();
+
+
+                // 응답을 읽습니다.
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code(1) - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+
+                    // 정상적인 응답 데이터
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+
+                    // 에러 발생
+
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+
+    public class FeatureTask extends AsyncTask<String, Void, String> {
+
+        private static final String TAG = "FeatureTask";
+
+        public Context mContext;
+
+        public FeatureTask(Context mContext) {
+            super();
+
+            this.mContext = mContext;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.d(TAG, "POST response(2)  - " + result);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않습니다.
+            String serverURL = (String) params[0];
+
+            // TODO : 아래 형식처럼 원하는 key과 value를 계속 추가시킬수있다.
+            // 1. PHP 파일을 실행시킬 수 있는 주소와 전송할 데이터를 준비합니다.
+            String user_email = (String) params[1];
+            String user_value = (String) params[2];
+            String user_date = (String) params[3];
+
+            // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
+            // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
+            // 여기에 적어준 이름을 나중에 PHP에서 사용하여 값을 얻게 됩니다.
+
+            // TODO : 위에 추가한 형식처럼 아래 postParameters에 key과 value를 계속 추가시키면 끝이다.
+            // ex : String postParameters = "name=" + name + "&country=" + country;
+            String postParameters = "email=" + user_email + "&value=" + user_value + "&fDate=" + user_date;
 
             Log.d(TAG, postParameters);
 
